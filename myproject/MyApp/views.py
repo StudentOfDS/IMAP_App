cbadd2d3fad3b5b1f986ae470066d5589f23486f
StudentOfDS <144333773+StudentOfDS@users.noreply.github.com>
2023-11-01 23:27:13 +0530

    Fourths Commit

--- myproject/MyApp/views.py
+++ myproject/MyApp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.urls import path 
+from . import views 
 
-# Create your views here.
+ 
+
+urlpatterns = [ 
+
+    path('my-view/', views.my_view, name='my-view'), 
+
+] 
+
+ 
+def my_view(request): 
+
+    return render(request, 'my_view.html')   
+
+ 
+urlpatterns = [ 
+
+    path('my-view/', views.my_view, name='my-view'), 
+
+] 
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = 'django-insecure-+%s@k&@5k4iz67-jv%q61a^9)fd9(i4vf1p=7h6j5!wg99acgo
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -56,7 +54,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': 'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -80,7 +77,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -100,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -112,11 +107,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
